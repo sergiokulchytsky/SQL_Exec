d868a5cb16112adda8cdd38f7cbf431ce2b2b82a
NICO <nico19@ukr.net>
2015-04-02 23:01:53 +0300

    changed form size

--- SQLDev/MainForm.Designer.cs
+++ SQLDev/MainForm.Designer.cs
@@ -49,7 +49,7 @@
             this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(449, 257);
+            this.richTextBox1.Size = new System.Drawing.Size(449, 117);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -59,7 +59,7 @@
             this.dataGridView.AllowUserToAddRows = false;
             this.dataGridView.AllowUserToDeleteRows = false;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(12, 275);
+            this.dataGridView.Location = new System.Drawing.Point(12, 135);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
             this.dataGridView.Size = new System.Drawing.Size(657, 236);
@@ -69,7 +69,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(681, 523);
+            this.ClientSize = new System.Drawing.Size(681, 386);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
