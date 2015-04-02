0bb99875d26fb0f2ea345b868daf66cc07cecd1d
Serhiy Kulchytsky <serhiyx@mail.ru>
2015-04-02 21:15:58 +0300

    First commit

---
+++ SQLDev/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SQLDev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(449, 257);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 275);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(657, 236);
+            this.dataGridView.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 523);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SQLDev";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ SQLDev/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace SQLDev
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection myConnection = new SqlConnection("user id=ser;" +
+                                   "password=123456;server=KITKAT;" +
+                                   "database=database;");
+        SqlCommand comm = new SqlCommand();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myConnection.Open();
+                comm.Connection = myConnection;
+                comm.CommandText = richTextBox1.Text.ToString();
+                using (SqlDataReader reader = comm.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        dataGridView.Visible = true;
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        dataGridView.DataSource = dt;
+                    }
+                    else
+                    {
+                        dataGridView.Visible = false;
+                    }
+                }
+                //comm.ExecuteNonQuery();
+                //MessageBox.Show("Successful!");
+                //richTextBox1.ResetText();
+                myConnection.Close();
+            }
+            catch (SqlException exept)
+            {
+                MessageBox.Show(exept.Message);
+                myConnection.Close();
+            }
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Regex rx = new Regex("INSERT ");
+            int index = richTextBox1.SelectionStart;
+
+            foreach (Match item in rx.Matches(richTextBox1.Text))
+            {
+                richTextBox1.Select(item.Index, item.Value.Length);
+                richTextBox1.SelectionColor = Color.Blue;
+                richTextBox1.SelectionStart = index;
+                richTextBox1.SelectionColor = Color.Black;
+            }
+        }
+    }
+}
