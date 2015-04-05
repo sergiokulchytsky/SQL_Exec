105532076ccf3bdd6806e50b6980e9e18faef48e
Serhiy Kulchytsky <serhiyx@mail.ru>
2015-04-05 14:14:04 +0300

    Update last commit

---
+++ DataLib/DB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace DataLib
+{
+    public class DB
+    {
+        SqlConnection myConnection = new SqlConnection("user id=ser; password=123456; database=AdventureWorksDW2014; server=KITKAT;");
+        SqlCommand comm = new SqlCommand();
+        public DataTable Read(String SQL)
+        {
+             myConnection.Open();
+                    comm.Connection = myConnection;
+                    comm.CommandText = SQL;
+                    using (SqlDataReader reader = comm.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(reader);
+                            myConnection.Close();
+                            return dt;
+                        }
+                        else
+                        {
+                            throw new Exception("NOT SELECT");
+                        }
+                    }
+                   
+        }
+    }
+
+}
---
+++ DataLib/TaskControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLib
+{
+    public class TaskControl
+    {
+        public string description;
+        public string imgPath;
+        public string correctSQL;
+    }
+}
